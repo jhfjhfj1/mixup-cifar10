db75e64bba6927d15103142fda4510dc7eacf590
Haifeng Jin <jhfjhfj1@gmail.com>
2018-11-20 19:57:14 -0600

    update

--- utils.py
+++ utils.py
@@ -43,8 +43,7 @@ def init_params(net):
                 init.constant(m.bias, 0)
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
+term_width = 8
 
 TOTAL_BAR_LENGTH = 86.
 last_time = time.time()
